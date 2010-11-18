868eec2f595d2bdb8d12284412a9dba47a4b52ec
Davide Bellettini <davide.bellettini@gmail.com>
2010-11-18 11:05:33 +0000

    GUI Application stub

---
+++ simplecrypt-gui/src/main/java/com/davidebellettini/crypt/gui/SimpleCryptApp.java
@@ -0,0 +1,19 @@
+package com.davidebellettini.crypt.gui;
+
+import org.jdesktop.application.SingleFrameApplication;
+
+public class SimpleCryptApp extends SingleFrameApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(SimpleCryptApp.class, args);
+	}
+
+	@Override
+	protected void startup() {
+		// TODO Auto-generated method stub
+	}
+
+}
